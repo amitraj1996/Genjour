9c0062f0fef2deca918fb4045f253eeaaaec755b
Injoker <amitraj.1996@gmail.com>
2017-08-28 23:56:01 +0530

    dynamic category header

--- category-header.php
+++ category-header.php
@@ -1,3 +1,14 @@
+<?php
+  include('config/connect.php');
+
+  // fetching categories from database
+
+  $query = "SELECT * FROM categories";
+  $categories =  $conn->query($query);
+
+?>
+
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -8,27 +19,32 @@
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Life</a>
+
+
+            <?php
+            //category in header 
+              while($row = $categories ->fetch_assoc()){
+
+            ?>
+              
+              <?php 
+                if(isset($_GET['category'] == $row["category_name"];)){
+              ?>
+              <li class="nav-item active">
+                <a class="nav-link" href="category/<?php echo $row["category_name"]; ?>"><?php echo $row["category_name"]; ?></a>
               </li>             
-              <li class="nav-item">
-                <a class="nav-link" href="#">Philosophy</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Controversy</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Politics</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Self Help</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Economics</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Socitey</a>
-              </li>
+              <?php }
+                else{
+
+                  ?>
+                  <li class="nav-item active">
+                  <a class="nav-link" href="category/<?php echo $row["category_name"]; ?>"><?php echo $row["category_name"]; ?></a>
+                  </li>
+                  <?php
+                }
+                } 
+
+              ?>
     </ul>
             <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
           <li class="nav-item">
@@ -38,7 +54,7 @@
             </li> 
           </ul>
 
-    <form class="form-inline my-2 my-lg-0">
+    <form class="form-inline my-2 my-lg-0" method="post">
       <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
--- config/connect.php
+++ config/connect.php
@@ -8,12 +8,12 @@ $db = "genjour";
 
 		// Create connection
 	$conn = new mysqli($servername, $username, $password,$db);
-		// Check connection
+		/* Check connection
 		if ($conn->connect_error) {
 		    die("Connection failed: " . $conn->connect_error);
 		} 
 		echo "Connected successfully";
 		
-
+	*/
 
 ?>

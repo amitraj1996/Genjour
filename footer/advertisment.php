c8ab8457c56032b72783eb5f96a2fe4bb04b64b8
Shikhar <shikarvaish@gmail.com>
2017-08-29 03:08:14 +0530

    Designing
    Added some design but the issue is that the footer file is leaving some
    space on both sides ..i.e right and left hence please see why

--- footer/advertisment.php
+++ footer/advertisment.php
@@ -2,9 +2,43 @@
 include ('routes.php');
 include ('header.php');
  ?>
+<!-- slider advertisment-->
+<div class="mt-3 ml-3 mr-3">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../img/svg/first-slide.svg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../img/svg/second-slide.svg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../img/svg/third-slide.svg" alt="Third slide">
+    </div>
+    <div class="carousel-item ">
+      <img class="d-block w-100" src="../img/svg/first-slide.svg" alt="Fourth slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+ <!-- cards for news -->
  <div class="container">
 
- 	<div class="mt-5 row">
+ 	<div class="mt-5 row mb-3">
  		<div class="card-deck">
  		  <div class="card orange">
  		    <img class="card-img-top" src="../img/svg/sample.svg" alt="Card image cap">

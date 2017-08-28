5717ad1d61d818f4cce81274961d3b6e87ec8564
Shikhar <shikarvaish@gmail.com>
2017-08-29 01:28:28 +0530

    intial changes
    I've made some initial changes since I don't exactly know what more to
    customise hence see and please tell me what else is needed

--- footer/advertisment.php
+++ footer/advertisment.php
@@ -2,6 +2,52 @@
 include ('routes.php');
 include ('header.php');
  ?>
+ <div class="container">
+
+ 	<div class="mt-5 row">
+ 		<div class="card-deck">
+ 		  <div class="card orange">
+ 		    <img class="card-img-top" src="../img/svg/sample.svg" alt="Card image cap">
+ 		    <div class="card-body">
+ 		      <h4 class="card-title">Post title</h4>
+ 		      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+ 		    </div>
+ 		    <div class="card-footer">
+ 		      <small class="text-muted">Last updated 3 mins ago</small>
+ 		    </div>
+ 	  	  </div>
+ 		  <div class=" card">
+ 		    <img class="card-img-top" src="../img/svg/sample.svg" alt="Card image cap">
+ 		    <div class="card-body">
+ 		      <h4 class="card-title">Post title</h4>
+ 		      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+ 		    </div>
+ 		    <div class="card-footer">
+ 		      <small class="text-muted">Last updated 3 mins ago</small>
+ 		    </div>
+ 		  </div>
+ 		    <div class="card">
+ 		    <img class="card-img-top" src="../img/svg/sample.svg" alt="Card image cap">
+ 		    <div class="card-body">
+ 		      <h4 class="card-title">Post title</h4>
+ 		      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+ 		    </div>
+ 		    <div class="card-footer">
+ 		      <small class="text-muted">Last updated 3 mins ago</small>
+ 		    </div>
+ 		  </div>
+ 		  <div class="card">
+ 		    <img class="card-img-top" src="../img/svg/sample.svg" alt="Card image cap">
+ 		    <div class="card-body">
+ 		      <h4 class="card-title">Post title</h4>
+ 		      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+ 		    </div>
+ 		    <div class="card-footer">
+ 		      <small class="text-muted">Last updated 3 mins ago</small>
+ 		    </div>
+ 		  </div>
+ 		</div>
+ 	</div>
 
 
 

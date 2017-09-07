6e36b5a62d68320c300eb99eca7ff0947b0574cb
Shikhar <shikarvaish@gmail.com>
2017-09-07 23:17:15 +0530

    adding interview page

---
+++ interview.php
@@ -0,0 +1,73 @@
+<!-- including header and routes file -->
+<?php
+include 'routes.php';
+include 'header.php';
+?>
+<!-- adding external styling elements -->
+<style media="screen">
+.sizing
+{
+
+}
+.back
+{
+  background-image: url('http://cdn.wallpapersafari.com/16/68/M4VEtx.jpg');
+}
+.bt
+{
+  align:right;
+}
+.titles
+{
+
+}
+</style>
+<!-- end of header inclusion -->
+<!-- column no 1 -->
+<div class="container mt-4 mb-4">
+<div class="row">
+  <div class="col-2">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title text-center">Previous</h4>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+<!-- column no 2 -->
+<div class="col-8">
+  <div class="card">
+    <div class="card-body">
+      <div class="card-img">
+        <h4 class=" back titles text-center">Interview <button type="button" class="bt btn btn-success float-right">Success</button>
+          <h6 class="text-muted float-bottom float-center">Date and time</h6>
+        </h4>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac condimentum justo, id vulputate sapien. Morbi posuere nibh ac ipsum mollis tempus. Morbi ac dignissim nibh. Quisque vel urna nunc. In non mollis risus, sit amet maximus metus. Integer eu pellentesque lorem, a pulvinar turpis. In blandit laoreet leo, in maximus diam tincidunt nec. Sed euismod viverra lorem at dignissim. Duis eros felis, dapibus a eleifend vitae, bibendum sit amet justo. Sed sed metus at nisl lobortis sagittis. Donec dignissim ex non imperdiet dapibus. Cras vulputate, felis non efficitur laoreet, elit tellus finibus augue, id dignissim erat arcu malesuada magna.
+
+Aliquam euismod est eu semper tincidunt. Vivamus varius ex eget ipsum fringilla dignissim. Aliquam sed leo a lorem blandit ultricies sit amet et tellus. Praesent id eleifend tellus, vitae sodales enim. Quisque eu erat gravida, volutpat nisi vestibulum, tristique mauris. Donec commodo porta enim eu fringilla. Phasellus vitae enim mollis nisl sodales lobortis. Praesent quis ornare purus. Integer lacinia enim eleifend nisi rutrum mollis. Morbi lectus eros, efficitur at sodales et, iaculis eget purus. Phasellus ut dignissim nulla. Nulla vestibulum mi ac commodo maximus. Suspendisse sit amet leo vitae augue semper laoreet vel at arcu. Mauris posuere convallis fermentum. Donec sollicitudin finibus nulla nec sodales. Integer commodo sagittis quam ut varius.
+
+Phasellus est metus, laoreet id dolor in, bibendum eleifend risus. Ut a consequat tellus. In tincidunt diam quis semper porttitor. Pellentesque consequat semper quam, et egestas arcu. Nulla sed elementum turpis. Praesent nec lorem turpis. Nulla blandit quis turpis eget imperdiet. Maecenas et scelerisque massa. Maecenas eget tincidunt nibh, et lobortis justo. Donec eget magna eget quam rhoncus varius. Nam a metus id orci mollis aliquet et eget lectus. Curabitur malesuada commodo tellus quis venenatis. Suspendisse posuere congue imperdiet. Vivamus ac neque id erat ornare sollicitudin a eu erat.
+
+Sed augue ligula, laoreet ac velit maximus, pharetra viverra purus. Etiam tortor turpis, vulputate eget mattis quis, pretium nec elit. Morbi dignissim pellentesque nunc id porta. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum hendrerit egestas eros, non finibus sem commodo eu. Etiam lobortis viverra fringilla. Integer nec sapien sollicitudin, euismod neque in, hendrerit est. Nunc vulputate ipsum eget erat egestas tincidunt. Suspendisse condimentum tellus sed dictum placerat.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- column no 3 -->
+  <div class="col-2">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title text-center">Upcoming</h4>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- including footer files -->
+<?php
+include 'footer.php';
+ ?>

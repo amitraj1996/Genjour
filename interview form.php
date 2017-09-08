1ea31f62605318b9eed1982932b0a79f35c13c51
Shikhar <shikarvaish@gmail.com>
2017-09-08 23:20:54 +0530

    added comments in the interview form page

--- interview form.php
+++ interview form.php
@@ -2,10 +2,11 @@
 include 'routes.php';
 include 'header.php';
  ?>
+<!-- ending the including of header and routes file -->
 
 
 
 
-
+<!-- starting to include the footer file -->
  <?php
  include 'footer.php'; ?>
